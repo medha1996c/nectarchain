4a2dcbaa814a01a8c3f949a281f96247324b3a23
medha1996c <chakrabortymedha907@gmail.com>
2026-03-06 18:32:27 +0100

    ENF added to NSB, Laser source calibration added

--- src/nectarchain/trr_verification_package/nsb_rate_calibration.py
+++ src/nectarchain/trr_verification_package/nsb_rate_calibration.py
@@ -245,7 +245,7 @@ def main():
 
     T_0 = 2.0  # Offset to be subtracted from N_SAMPLES
     NSB_rate = (pow(pedestal_std, 2) - Dark_std) / (
-        pow(GAIN_DEFAULT, 2) * (N_SAMPLES - T_0) * pow(10, -9)
+        pow(GAIN_DEFAULT * ENF, 2) * (N_SAMPLES - T_0) * pow(10, -9)
     )
 
     NSB_rate_mean = (np.nanmean(NSB_rate, axis=1)) * pow(10, -9)
@@ -288,9 +288,9 @@ def main():
     c_rounded = round(c_scaled, dec_c)
 
     s = (
-        rf"$m = ({m_rounded} \pm {m_err_rounded})\times 10^{{{exp_m}}}$"
+        rf"$m = ({m_rounded} \pm {m_err_rounded})\times 10^{{{exp_m}}}$ GHz/mA"
         "\n"
-        rf"$c = ({c_rounded} \pm {c_err_rounded})\times 10^{{{exp_c}}}$"
+        rf"$c = ({c_rounded} \pm {c_err_rounded})\times 10^{{{exp_c}}}$ GHz"
     )
     ax.text(
         0.05,
